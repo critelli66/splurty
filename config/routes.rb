5c7ee454dcb9310b2453d71cadf53879f1868f2d
Michael Critelli <mcritelli25@aol.com>
2015-05-31 20:10:04 +0000

    Building the about page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Splurty::Application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'quotes#index'
 
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
@@ -13,6 +14,7 @@ Splurty::Application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   resources :quotes
+  get 'about', :to => 'quotes#about'
 
   # Example resource route with options:
   #   resources :products do
