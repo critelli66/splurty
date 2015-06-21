5d9f5f385080742e43ac38e4bff8afeb10f3b445
Michael Critelli <mcritelli25@aol.com>
2015-06-21 00:40:53 +0000

    Added Factory Girl

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class QuoteTest < ActiveSupport::TestCase
 
 	test "unique_tag" do 
-		quote = Quote.create(:author => 'Michael Critelli', :saying => 'Testing Application.')
+		quote = FactoryGirl.create(:quote, :author => 'Michael Critelli')
 		expected = 'MC#' + quote.id.to_s
 		actual = quote.unique_tag
 		assert_equal expected, actual
